8e8daa74a290b68b361f75bfe6341e07ce4a871c
Leorne <49460319+Leorne@users.noreply.github.com>
2019-10-04 04:03:36 +0300

    Initial Commit

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,18 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-
 use App\Http\Controllers\FileController;
 
 Route::get('/', function () {
-    return redirect('/main');
-});
+    return view('files.main');
+})->name('main');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/main', 'FileController@main')->name('main');
 Route::post('/main/upload', 'FileController@store');
-Route::get('/main/list','FileController@index')->name('list');
-Route::get('/main/list/{file}','FileController@show');
+Route::get('/main/list', 'FileController@index')->name('list');
+Route::get('/main/list/{file}', 'FileController@show');
 Route::delete('/main/list/{file}', 'FileController@destroy');
 
 Route::post('/main/list/{file}/reply', 'ReplyController@store');
